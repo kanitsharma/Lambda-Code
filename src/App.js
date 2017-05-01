105ec763321f4505270409fa1826ced9ca61bcb5
kanit sharma <kanitshrm09@gmail.com>
2017-05-01 07:06:44 +0530

    Firebase suppoer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import Toolbar from './components/toolbar';
 import Editor from './components/editor';
 import Footer from './components/footer';
+import * as firebase from 'firebase';
 import './App.css';
 
+
 class App extends Component {
+
   constructor(props){
     super(props)
     this.state = {
       value:"/*Enter Your Javascript Code. \nUse functions like alert,prompt or console.log to check the outputs. \nClick the Execute Button to execute your code. \nCheck gutter for linting. \nYour code will be automatically transpiled to es5 when your execute it.*/",
       theme:"ambiance",
-      change:true
+      change:true,
+      uid : '',
     }
     this.onchange = this.onchange.bind(this)
     this.execute = this.execute.bind(this)
@@ -18,8 +22,47 @@ class App extends Component {
     this.empty = this.empty.bind(this)
     this.transpile = this.transpile.bind(this)
     this.evalit = this.evalit.bind(this)
+    this.update = this.update.bind(this)
+  }
+  componentWillMount(){
+    if(!localStorage.uid){
+      console.log('logging in')
+      firebase.auth().signInAnonymously().catch(function(error) {
+        if(error) console.log(error.message)
+      })
+    }
+    firebase.auth().onAuthStateChanged(function(user) {
+
+      if (user) {
+        localStorage.uid = user.uid
+        this.update()
+      } else {
+        console.log('User logged out')
+
+      }
+      // ...
+    }.bind(this));
+    this.setState({ uid : localStorage.uid })
+
+    if(localStorage.uid){
+      if(!localStorage.value){
+        firebase.database().ref().child('users/'+localStorage.uid).set({
+            "value" : this.state.value
+        })
+      }
+      const dbref = firebase.database().ref().child('users/'+localStorage.uid)
+      dbref.on('value' , snap => {
+        this.setState({ value : snap.val().value })
+      })
+    }
+
+  }
+  update(){
+
   }
   onchange(code){
+    localStorage.value = code
+    firebase.database().ref().child('users/'+localStorage.uid+'/value').set(code)
     this.setState({ value : code })
   }
   transpile(value , callback){
@@ -31,8 +74,10 @@ class App extends Component {
         this.setState({ value : code } , this.evalit)
     })
   }
-  evalit = () => {
-    eval(this.state.value)
+  evalit = (code = this.state) => {
+    setTimeout(()=>{
+      eval(code.value)
+    },10)
   }
   changetheme(value){
     this.setState({ theme : value })
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as firebase from 'firebase';
 import './index.css';
+
+var config = {
+  apiKey: "AIzaSyBSdKT9TMbs3DQtHecs52BbSo4pN4sFRnc",
+  authDomain: "lambda-code-bcb9a.firebaseapp.com",
+  databaseURL: "https://lambda-code-bcb9a.firebaseio.com",
+  projectId: "lambda-code-bcb9a",
+  storageBucket: "lambda-code-bcb9a.appspot.com",
+  messagingSenderId: "422976253303"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
