620d242066e5ce42b01b6af5a54a052d05f5cf96
kanit sharma <kanitshrm09@gmail.com>
2017-05-04 23:59:58 +0530

    Responsive Fix2

--- src/components/popup.js
+++ src/components/popup.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Popup = props => {
   return(
-    <div className="popup">
-      <div className="footer"><p> Connect to your friend by sharing this Special key </p></div>
-      <div><p>{props.uid}</p></div>
-      <div><button className="but" onClick={props.closepop}>Ok</button></div>
+    <div className="overlay">
+      <div className="popup">
+        <div className="footer"><p> Connect to your friend by sharing this Special key </p></div>
+        <div><p>{props.uid}</p></div>
+        <div><button className="but" onClick={props.closepop}>Ok</button></div>
+      </div>
     </div>
   )
 }
